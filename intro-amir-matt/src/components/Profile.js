885e580d2a85160d690041b0a4f34f716a8fcc1d
Rashadjaxon <amirjacksonscholar@gmail.com>
2024-02-15 15:47:50 -0800

    i hand it off to you

---
+++ intro-amir-matt/src/components/Profile.js
@@ -0,0 +1,15 @@
+
+
+const Profile = () => {
+    return (
+        <>
+         <h2>Information </h2> 
+            <h3>Amir</h3>
+                <p>Amirs hobbies are working out, poetry, 3d Printing, and video games. Don't touch his food or else...</p>   
+            <h3>Matt</h3>
+
+        </>
+    )
+}
+
+export default Profile
