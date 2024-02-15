09eddb70a7d31bda7d961abe01f1d375d408b24a
smorgannicole <morgann.smithh@yahoo.com>
2024-02-15 15:18:51 -0800

    1st drive

---
+++ intro-morgan-ryan/src/components/Profile.js
@@ -0,0 +1,23 @@
+
+const Profile = () => {
+    return (
+        <>
+            <ul>
+                <li>Name: Morgan Smith</li>
+                <li>Birthday: 4/13/1998</li>
+                <li>Hobbies: boba and chess enthusiast</li>
+                <li>Fun fact: </li>
+                <li>Favorite quote: </li>
+            </ul>
+            <ul>
+                <li>Name: Ryan Lemus</li>
+                <li>Birthday: 6/24/1993</li>
+                <li>Hobbies: golf afficionado, being a dad, and cooking</li>
+                <li>Fun fact: color blind</li>
+                <li>Favorite quote: ""You miss 100% of shots that you don't take" -Wayne Gretzky" -Michael Scott</li>
+            </ul>
+        </>
+    )
+}
+
+export default Profile
