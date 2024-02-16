1c5c7bbf15ca744f8b5e108a3975d2bdffca0ddb
mark19242 <mark19242@gmail.com>
2024-02-15 16:12:41 -0800

    mvp

---
+++ intro-mark-judas/src/components/Header.js
@@ -0,0 +1,6 @@
+const Header = () => {
+    return <h1>Favorite Things</h1>
+}
+
+export default Header
+
---
+++ intro-mark-judas/src/components/Profile.js
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return (
+        <>
+        <p> Mark & Judas
+            Are Veterans Both Learning To Code!
+        </p>
+        </>
+    )
+}
+
+export default Profile
