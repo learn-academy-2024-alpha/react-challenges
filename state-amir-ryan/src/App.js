e770310984905556f49491fd4e4cd4700baee975
Rlemus93 <rlemus93@gmail.com>
2024-02-20 21:24:43 -0800

    tried to finish stretch goals

--- state-amir-ryan/src/App.js
+++ state-amir-ryan/src/App.js
@@ -1,25 +1,34 @@
- import { useState } from "react"
-import "./App.css"
+  import { useState } from "react"
+  import "./App.css"
+  import React from "react";
+  import Square from "./Components/Square";
  
  
  const App = () => {
-  const [color, setColor] = useState(0) 
-  const colors = ["Green", "Purple", "Yellow", "Blue"]
-  const handleClick = () => {
-if (color <= 4) {
-  setColor(color + 1)
-}
-  } 
+   const [boxes, setBoxes] = useState([])
+
+   const addBox = () => {
+   setBoxes([...boxes, <Square key={boxes.length} />])
+   
+  }
+
+  const removeBox = () => {
+    if (boxes.length > 0){
+      const newBoxes = [...boxes]
+      newBoxes.pop()
+      setBoxes(newBoxes)
+    }
+  }
+
   return (
     <>
       <h1>Good Times With Color Boxes</h1>
-      <div className="colors" onClick={handleClick} className="counter-box">
-        White
-      </div>
+      {boxes}
+      <button onClick={addBox}> Add Box</button>
+      <button onClick={removeBox}> Remove Box</button>
     </>
   )
- } 
-
+  }
 
 
 export default App;
