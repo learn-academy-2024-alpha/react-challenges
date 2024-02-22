16b7e5f7ca82263d993d110475f2a74446d43128
MattBarnabo <mattbarnabo@gmail.com>
2024-02-22 16:01:01 -0600

    adds styling

---
+++ props-amir-matt/src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Dice Game</h1>
+    <div className='dice-game'>
+      
+      
+    
+      <div className='dice-box'>
+
+       <h4 className='dice-text'>Dice</h4>
+       <p>CLICK BOX TO ROLL</p>
+
+        <button 
+        className='dice'>
+
+        </button>
+      </div>
+
+      <div className='roll-box'>
+      <h4 className='roll-text'>Rolls</h4>
+
+      </div>
+    
+    </div>
+    
+    </>
+
+  );
+}
+
+export default App;
