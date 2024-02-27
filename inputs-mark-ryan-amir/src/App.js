e2b411c938143e2632916267422b992f41209bfc
mark19242 <mark19242@gmail.com>
2024-02-27 13:29:18 -0800

    question 1 and 2 completed

---
+++ inputs-mark-ryan-amir/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import GoodRobot from './Component/GoodRobot';
+import BadRobot from './Component/BadRobot';
+
+const App = () => {
+  
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  }
+  
+  
+  return (
+    <>
+    <h1>TALKO BOT ~BEE BOP BEE BO~</h1>
+    <label>
+      Say something
+      </label>
+      <input type="text" value={userInput} onChange={handleChange}/>
+      <GoodRobot userInput={userInput}/>
+      <BadRobot userInput={userInput}/>
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
